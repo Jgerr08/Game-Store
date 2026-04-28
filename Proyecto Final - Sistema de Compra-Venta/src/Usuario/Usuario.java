41a514869939a26488f041e5abad91a3ade781d1
Jgerr08 <jesger2008@gmail.com>
2026-04-28 08:48:01 -0600

    Hello World

---
+++ Proyecto Final - Sistema de Compra-Venta/src/Usuario/Usuario.java
@@ -0,0 +1,11 @@
+package Usuario;
+
+public class Usuario {
+    String id;
+    String contraseña;
+
+    public Usuario(String id, String contraseña){
+        this.id = id;
+        this.contraseña = contraseña;
+    }
+}
