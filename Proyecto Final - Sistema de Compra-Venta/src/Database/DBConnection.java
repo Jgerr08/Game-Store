6ea1c2116d4847a5fe9eb8b61745fb4d284eb7d9
Jgerr08 <jesger2008@gmail.com>
2026-06-12 20:31:41 -0600

    h

--- Proyecto Final - Sistema de Compra-Venta/src/Database/DBConnection.java
+++ Proyecto Final - Sistema de Compra-Venta/src/Database/DBConnection.java
@@ -1,12 +1,11 @@
 package Database;
 
+import Juegos.Juego;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import Juegos.Juego;
 
 
 public class DBConnection {
